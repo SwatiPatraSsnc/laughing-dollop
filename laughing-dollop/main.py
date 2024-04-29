d1cfb0660bf126cd0e5e2060d226e1fa895e65ec
swati.patra <swati.patra@sscinc.com>
2024-04-29 14:32:33 +0530

    Initial Commit

---
+++ laughing-dollop/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = [['John', 45, 'Manager'], ['lisa', 34, 'Developer'], ['Thomas', 36, 'Tester'], ['Kaisy', 23, 'Tester'], ['Peter', 45, 'Manager'],
+        ['Kindle', 45, None]]
+
+df = pd.DataFrame(data, columns=['Name', 'age', 'Role'])
+
+df.dropna(axis=0, inplace=True)
+
+print(df.head(3))
+
+
